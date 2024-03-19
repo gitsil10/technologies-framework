f357e6fc0ee5377a14677b9610903be9812ee52b
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-19 00:03:22 -0400

    chore package

--- react-flask-apps/time-web-app/api/api.py
+++ react-flask-apps/time-web-app/api/api.py
@@ -1,7 +1,11 @@
-import time
+import time, datetime
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def confirm_run():
+    return "api running"
 
 @app.route("/time")
 def get_current_time():
